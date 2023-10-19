b2ead519becebc2af0fe8b540c26d7e4032281b3
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2023-10-19 09:42:13 -0300

    Add files via upload

---
+++ Calculadora/Program.cs
@@ -0,0 +1,49 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Calculadora");
+        Console.Write($"Digite um valor: ");
+        float n1 = float.Parse(Console.ReadLine());
+        Console.WriteLine("Agora digite +(soma), -(subtração), /(divisão) ou *(multiplicação) para continuarmos: ");
+        string op = Console.ReadLine();
+        if (op == "+")
+        {
+            Console.Write("Digite outro valor: ");
+            float n2 = float.Parse(Console.ReadLine());
+            double r = n1 + n2;
+            Console.WriteLine($"A soma entre os valores {n1} e {n2} é igual a: {Math.Round(r,2)}");
+        }
+        if (op == "-")
+        {
+            Console.Write("Digite outro valor: ");
+            float n2 = float.Parse(Console.ReadLine());
+            double r = n1 - n2;
+            Console.WriteLine($"A subtração entre os valores {n1} e {n2} é igual a: {r}");
+        }
+        if (op == "/")
+        {
+            Console.Write("Digite outro valor: ");
+            float n2 = float.Parse(Console.ReadLine());
+            double r = n1 / n2;
+            Console.WriteLine($"A divisão entre os valores {n1} e {n2} é igual a: {r}");
+        }
+        if (op == "*")
+        {
+            Console.Write("Digite outro valor: ");
+            float n2 = float.Parse(Console.ReadLine());
+            double r = n1 * n2;
+            Console.WriteLine($"A multiplicação entre os valores {n1} e {n2} é igual a: {r}");
+        }
+        Console.WriteLine("Deseja repetir o programa?(Responda com s(sim) ou n(não)): ");
+        string Sn = Console.ReadLine();
+        if (Sn == "s") 
+        {
+            Main();
+        }
+        else
+        {
+
+        }
+    }
+}
